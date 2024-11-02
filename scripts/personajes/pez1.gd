5f6b3ac3bbd8fa7b78daa242e9dc7138f5406737
Dsanz75 <dsanz@infinet.es>
2024-11-02 22:00:20 +0100

    Version definitiva peces
    Cada pez aparece a partir de una determinada profundidad

---
+++ scripts/personajes/pez1.gd
@@ -0,0 +1,48 @@
+# PEZ PERSEGUIDOR NIVEL 1 - Davidas
+
+extends CharacterBody2D
+
+@export var speed = 200
+
+var target = position 
+
+func _physics_process(_delta):
+
+	target   = %Patricio.position
+	target.x = %Patricio.position.x
+	target.y = %Patricio.position.y
+		
+	
+	velocity = position.direction_to(target) * speed
+	
+	if position.distance_to(target) > 10:
+		
+			if position.x < target.x && target.x - position.x > 50:
+					$AnimatedSprite2D.flip_h = false
+					$AnimatedSprite2D.play("idle")				
+					
+			else: if position.x < target.x && target.x - position.x < 50:	
+					$AnimatedSprite2D.flip_h = false
+					$AnimatedSprite2D.play("girando")
+										
+			if position.x > target.x && position.x - target.x > 50:			
+					$AnimatedSprite2D.flip_h = true
+					$AnimatedSprite2D.play("idle")				
+				
+			else: if position.x > target.x && position.x - target.x < 50:
+					$AnimatedSprite2D.flip_h = true
+					$AnimatedSprite2D.play("girando")
+						
+			move_and_slide()	
+	
+	else:  if position.distance_to(target) < 10:
+		
+				if position.x < target.x:	
+					$AnimatedSprite2D.flip_h = false
+					$AnimatedSprite2D.play("cabreado")
+				if position.x > target.x:	
+					$AnimatedSprite2D.flip_h = true
+					$AnimatedSprite2D.play("cabreado")
+				%Patricio.remove_health()
+		
+				
